9b48807cfb5b46cb2f9455f458b85c1df26b3c60
alwaysalty <cenkerdonmez@gmail.com>
2023-06-09 18:33:48 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
 // Your code here
+// createEmployeeRecord
+function createEmployeeRecord(employeeData) {
+  return {
+    firstName: employeeData[0],
+    familyName: employeeData[1],
+    title: employeeData[2],
+    payPerHour: employeeData[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  };
+}
+
+// createEmployeeRecords
+function createEmployeeRecords(employeesData) {
+  return employeesData.map(createEmployeeRecord);
+}
+
+// createTimeInEvent
+function createTimeInEvent(employee, dateStamp) {
+  const [date, hour] = dateStamp.split(" ");
+  employee.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour),
+    date: date
+  });
+  return employee;
+}
+
+// createTimeOutEvent
+function createTimeOutEvent(employee, dateStamp) {
+  const [date, hour] = dateStamp.split(" ");
+  employee.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour),
+    date: date
+  });
+  return employee;
+}
+
+// hoursWorkedOnDate
+function hoursWorkedOnDate(employee, date) {
+  const timeInEvent = employee.timeInEvents.find(event => event.date === date);
+  const timeOutEvent = employee.timeOutEvents.find(event => event.date === date);
+  return (timeOutEvent.hour - timeInEvent.hour) / 100;
+}
+
+// wagesEarnedOnDate
+function wagesEarnedOnDate(employee, date) {
+  const hoursWorked = hoursWorkedOnDate(employee, date);
+  return hoursWorked * employee.payPerHour;
+}
+
+// allWagesFor
+function allWagesFor(employee) {
+  const datesWorked = employee.timeInEvents.map(event => event.date);
+  const totalWages = datesWorked.reduce((sum, date) => sum + wagesEarnedOnDate(employee, date), 0);
+  return totalWages;
+}
+
+// calculatePayroll
+function calculatePayroll(employees) {
+  return employees.reduce((sum, employee) => sum + allWagesFor(employee), 0);
+}
